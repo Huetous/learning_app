68b562c15e8c90c9415bc0e0d7c8dd32aaf9290e
daddudota22 <daddudota22@mail.ru>
2020-03-11 21:35:08 +0700

    deleting

--- UnitSecond.ts
+++
@@ -1,7 +0,0 @@
-import {getSecondRStest} from "./Tasks/UnitSecond/secondRS";
-
-export class UnitSecond {
-    static getSecondRStest(test_id: number){
-        return getSecondRStest(test_id);
-    }
-}
